2603b60f879e55547377d0c364443fe3d5dd9d49
Ács Dániel <acsi@444.hu>
2014-06-17 14:26:12 +0200

    initial publish

---
+++ scribe-plugin-align-command.js
@@ -0,0 +1,39 @@
+define("scribe-plugin-align-command",
+  ["scribe-common/element","exports"],
+  function(__dependency1__, __exports__) {
+    "use strict";
+    var Element = __dependency1__;
+
+    __exports__["default"] = function(align) {
+        return function(scribe) {
+            var capitaliseFirstLetter = function(string) {
+                    return string.charAt(0).toUpperCase() + string.slice(1);
+            };
+
+            var commandName = 'align' + capitaliseFirstLetter(align);
+            var alignCommand = new scribe.api.Command('justify' + capitaliseFirstLetter(align));
+
+            alignCommand.execute = function() {
+                var selection = new scribe.api.Selection();
+                var range = selection.range;
+
+                var parentNode = selection.getContaining(function(node) {
+                    return Element.isBlockElement(node);
+                }.bind(this));
+
+                parentNode.style.textAlign = align;
+            };
+
+            alignCommand.queryState = function() {
+                var selection = new scribe.api.Selection();
+                var parentNode = selection.getContaining(function(node) {
+                    return Element.isBlockElement(node);
+                }.bind(this));
+
+                return !!parentNode && parentNode.style.textAlign === align;
+            };
+
+            scribe.commands[commandName] = alignCommand;
+        };
+    }
+  });
